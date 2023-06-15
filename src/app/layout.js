3e16b12a83ba8dc1d79b5e12296c251ed8c81bc2
santicammarano <santiago.cammarano@widergy.com>
2023-06-15 10:22:36 -0300

    feat: route

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,12 +3,13 @@ import { Route, Switch, Redirect } from 'react-router-dom';
 import { ConnectedRouter } from 'connected-react-router';
 
 import { history } from 'redux/store';
-import { HOME, BILLS_HISTORY } from 'constants/routes';
+import { HOME, BILLS_HISTORY, ACCOUNTS_LIST } from 'constants/routes';
 
 import Topbar from './components/Topbar';
 import Home from './screens/Home';
 import BillsHistory from './screens/BillsHistory';
 import styles from './styles.module.scss';
+import AccountsList from './screens/AccountsList';
 
 const App = () => (
   <div className={styles.fullContainer}>
@@ -18,6 +19,7 @@ const App = () => (
         <Switch>
           <Route exact path={HOME} component={Home} />
           <Route exact path={BILLS_HISTORY} component={BillsHistory} />
+          <Route exact path={ACCOUNTS_LIST} component={AccountsList} />
           <Route render={() => <Redirect to={HOME} />} />
         </Switch>
       </ConnectedRouter>
